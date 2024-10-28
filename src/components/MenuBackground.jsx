7057ba0b8a7511e9b85e4a37952ad00b6bf8c7ff
a-2280 <khaliff.williamss@gmail.com>
2024-10-28 12:41:14 -0700

    fixed centering issues on the navbar and about page

--- src/components/MenuBackground.jsx
+++ src/components/MenuBackground.jsx
@@ -1,6 +1,6 @@
 export default function MenuBackground() {
     return(
-        <div className="fixed top-0 w-svw h-[35svh] flex justify-center items-center pointer-events-none">
+        <div className="fixed top-0 w-svw h-[20rem] flex justify-center items-center pointer-events-none">
             <div className="hidden md:flex justify-center fixed text-[#C8C8C8] w-[60svw]">
                 <p className="text-[3.25svw] whitespace-nowrap">Lily Ballif is a photographer based in Utah, USA.</p>
             </div>
